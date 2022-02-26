ec1add9794af5f414e489c81e83d3d3b7eb4bc7f
MindXL <1634300602@qq.com>
2022-02-27 01:04:11 +0800

    feat: test.sh can print assembly code now with option -s

--- test.sh
+++ test.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
 cd cmake-build-debug || exit
-make
+make || exit
 
-./ccc "$1" > tmp.s
+separate() {
+  echo -e "\033[33m----------------------------------------\033[39m"
+}
+
+separate
+echo "Enter source code below:"
+separate
+read -r source
+separate
+
+result_s=$(./ccc "${source}")
+
+while getopts "s" opt; do
+  case ${opt} in
+  s)
+    echo "${result_s}"
+    separate
+    ;;
+  *)
+    echo "unknown option"
+    exit
+    ;;
+  esac
+done
+
+echo "${result_s}" >tmp.s
 clang tmp.s ../driver.c
-echo -e "\033[33m----------------------------------------\033[39m"
 ./a.out
 
 rm tmp.s a.out
